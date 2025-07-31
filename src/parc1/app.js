9eebb1fe715505e163f22bbb8b48df5f16df1f06
Malek <m.abdelmalek.dorbani@gmail.com>
2025-07-31 11:34:21 +0200

    Parc 2 initialisation - npm init + npm install express

--- src/parc1/app.js
+++ src/parc1/app.js
@@ -8,7 +8,6 @@ app.get('/dinosaurs', (req, res) => {
   res.json([{ id: 1, name: 'T‑Rex' }]);
 });
 
-// etc.
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Parc1 API listening on ${port}`));
